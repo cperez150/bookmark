44d06fdb2a054ad4380c49a417a06afd43d97561
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-11-19 17:19:44 -0500

    edit route working

--- bookmark_react/src/App.js
+++ bookmark_react/src/App.js
@@ -3,9 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import NewBookmark from "./components/NewBookmark.js";
-
+import EditBookmark from "./components/EditBookmark.js";
 import "./css/normalize.css";
-import "./css/skeleton.css";
+// import "./css/skeleton.css";
 import "./css/index.css";
 const baseURL = "http://localhost:3003";
 
@@ -19,6 +19,7 @@ class App extends Component {
     this.handleAddBookmark = this.handleAddBookmark.bind(this);
     this.deletedbookmark = this.deletedbookmark.bind(this);
     this.getBookmarks = this.getBookmarks.bind(this);
+    this.handleEditBookmark = this.handleEditBookmark.bind(this);
   }
   async componentDidMount() {
     this.getBookmarks();
@@ -28,8 +29,7 @@ class App extends Component {
     const data = response.data.foundBookmarks;
 
     this.setState({
-      bookmarks: data,
-      apiIsLoaded: true
+      bookmarks: data
     });
     console.log(data);
   }
@@ -39,7 +39,21 @@ class App extends Component {
       bookmarks: [...this.state.bookmarks, bookmark]
     });
   }
-
+  //EDIT
+  handleEditBookmark(bookmrk) {
+    const id = bookmrk._id;
+    console.log("this is the new book mark" + bookmrk.title);
+    const filteredBookmarks = this.state.bookmarks.map(bookmark => {
+      if (bookmark._id === id) {
+        return bookmrk;
+      } else {
+        return bookmark;
+      }
+    });
+    this.setState({
+      bookmarks: filteredBookmarks
+    });
+  }
   //DELETE
   async deletedbookmark(id) {
     await axios.delete(`${baseURL}/bookmarks/${id}`);
@@ -60,13 +74,18 @@ class App extends Component {
           {this.state.bookmarks.map(bookmark => {
             return (
               <div key={bookmark._id} className="li_div">
-                <li>
+                <p>
                   <a href={bookmark.url} target="_blank">
                     {bookmark.title}
                   </a>
-                </li>
+                </p>
+
                 <p onClick={() => this.deletedbookmark(bookmark._id)}>X</p>
-                <p>edit</p>
+                {/* <p onClick={this.editBookmark}>edit</p> */}
+                <EditBookmark
+                  bookmark={bookmark}
+                  handleEditBookmark={this.handleEditBookmark}
+                />
               </div>
             );
           })}
---
+++ bookmark_react/src/components/EditBookmark.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import axios from "axios";
+const baseURL = "http://localhost:3003";
+
+class EditBookmark extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "",
+      url: ""
+    };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleURLChange = this.handleURLChange.bind(this);
+  }
+  handleTitleChange(event) {
+    this.setState({ title: event.currentTarget.value });
+  }
+  handleURLChange(event) {
+    this.setState({ url: event.currentTarget.value });
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    const Title = this.state.title
+      ? this.state.title
+      : this.props.bookmark.title;
+
+    const URL = this.state.url ? this.state.url : this.props.bookmark.url;
+    const response = await axios.put(
+      `${baseURL}/bookmarks/${this.props.bookmark._id}`,
+      {
+        title: Title,
+        url: URL
+      }
+    );
+
+    this.setState({ title: "", url: "" });
+    this.props.handleEditBookmark(response.data);
+  }
+  render() {
+    return (
+      <div className="editForm">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="title"></label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={this.handleTitleChange}
+            value={this.state.title}
+            placeholder={this.props.bookmark.title}
+          />
+          <label htmlFor="url"></label>
+          <input
+            type="text"
+            id="url"
+            name="url"
+            onChange={this.handleURLChange}
+            value={this.state.url}
+            placeholder={this.props.bookmark.url}
+          />
+          <input type="submit" value="Edit Bookmark" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EditBookmark;
